6ac4c7366b270b4e238cf7c4a6d7f31beab89969
Ramonet72 <ramon.nogue@gmail.com>
2020-04-03 22:50:17 +0200

    panel detailed information

--- js/race.js
+++ js/race.js
@@ -42,6 +42,8 @@ if (!mobileClient) {
     document.getElementById("paypalImg").style.width = "200px";
 }
 
+document.getElementById("cplayer").style.fontSize = (20*factor) + "px";
+
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var cw = canvas.width;
@@ -72,14 +74,36 @@ function toggleModal() {
     modal.classList.toggle("show-modal");
 }
 
+
+
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+// Get the modal
+var modalDetails = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closeDetails")[0];
+
+span.onclick = function () {
+    modalDetails.style.display = "none";
+}
+
+window.onclick = function (event) {
+    // if (event.target == modalDetails) {
+    if (event.target.Name == undefined) modalDetails.style.display = "none";
+    // }
+}
+
+
 function windowOnClick(event) {
     if (event.target === modal) {
         toggleModal();
     }
 }
-
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
 
 function handleMouseMove(e) {
     // tell the browser we're handling this event
@@ -105,6 +129,39 @@ function handleMouseMove(e) {
 
 }
 
+function clicked() {
+    // alert(element.FirstName);
+    //var country = mapping[arguments[0].currentTarget.Name];
+
+    countryData = FullData.find(element => element.country == arguments[0].target.Name);
+
+    //var element = Object.values(mapping).find(element=> element.FirstName=arguments[0].currentTarget.id);
+
+    // modelDatails.style.top = "200px";
+    // modelDatails.style.left = "200px";
+    // modalDetails.style.paddingTop = (wheight / 2) + "px";
+    document.getElementById("dtitle").style.fontSize = (25 * factor) + "px";
+
+    var table = document.getElementById("details");
+    table.style.fontSize = (20 * factor) + "px";
+    table.rows[0].cells[1].innerHTML = countryData.country;
+    table.rows[1].cells[1].innerHTML = countryData.cases;
+    table.rows[2].cells[1].innerHTML = countryData.todayCases;
+    table.rows[3].cells[1].innerHTML = countryData.deaths;
+    table.rows[4].cells[1].innerHTML = countryData.todayDeaths;
+    table.rows[5].cells[1].innerHTML = countryData.recovered;
+    table.rows[6].cells[1].innerHTML = countryData.active;
+    table.rows[7].cells[1].innerHTML = (Math.round(countryData.deaths / countryData.cases * 10000) / 100).toFixed(2);
+    table.rows[8].cells[1].innerHTML = countryData.casesPerOneMillion;
+    table.rows[9].cells[1].innerHTML = countryData.deathsPerOneMillion;
+
+    document.getElementById("th1").style.paddingRight = (260*factor) + "px";
+    modalDetails.style.top = (wheight / 2) + "px";
+    modalDetails.style.display = "block";
+    modalDetails.style.left = (wwidth / 2 - modalDetails.offsetWidth / 2) + "px";
+
+}
+
 function showViewport() {
     var wwidth = $(window).width();
     var wheight = $(window).height();
@@ -120,7 +177,8 @@ function IsMobile() {
     (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
     return check;
 };
-
+var wwidth;
+var wheight;
 var scoreposy;
 var scoreposx;
 var scoreposy2;
@@ -175,8 +233,8 @@ function startRace() {
             return 0;
         });
 
-        var wwidth = $(window).width();
-        var wheight = $(window).height();
+        wwidth = $(window).width();
+        wheight = $(window).height();
         var circuitLength = wwidth - 80;
 
         var yinc = (wheight / 2 + yMargin) / numriders;
@@ -193,7 +251,7 @@ function startRace() {
         }
 
         if (restartRip) {
-           // posRip = Math.floor((Math.random() * 11));
+            // posRip = Math.floor((Math.random() * 11));
             ripPosition = 0;
             restartRip = false;
         }
@@ -243,16 +301,17 @@ function startRace() {
                 //     arrayTakenGIF[country.Index] = true;
                 posx = (position[i] + xMargin);
                 element = document.getElementById(country.FirstName);
+                element.Name = FullData[i].country;
                 posy = (yMargin + wheight / 2 + yinc * i) - element.height / 2 + 60;
 
-                element.style.zIndex = i+10;
+                element.style.zIndex = i + 10;
                 element.style.left = posx + "px";
                 //   document.getElementById("horse" + (i+1)).style.top =  1*i + "px";
                 element.parentElement.style.top = posy + "px";
                 if (FullData[i].country == "Italy") {
                     //if (document.getElementById("horse2" + (i + 1)) != null) {
                     document.getElementById("horse2" + (country.Index)).style.left = (position[i] + xMargin - 150) + "px";
-                    document.getElementById("horse2" + (country.Index)).style.zIndex = i+10;
+                    document.getElementById("horse2" + (country.Index)).style.zIndex = i + 10;
                     document.getElementById("horse2" + (country.Index)).parentElement.style.top = ((yMargin + wheight / 2 + yinc * i) - document.getElementById("horse2" + (country.Index)).height / 2 + 45) + "px";
                 }
             }
@@ -262,8 +321,9 @@ function startRace() {
                 while (arrayTakenGIF[j]) j = (j) % 11 + 1;
                 posx = (position[i] + xMargin);
                 element = document.getElementById("horse" + (j));
+                element.Name = FullData[i].country;
                 posy = (yMargin + wheight / 2 + yinc * i) - element.height / 2 + 70;
-                element.style.zIndex = i+10;
+                element.style.zIndex = i + 10;
                 element.style.left = posx + "px";
                 //   document.getElementById("horse" + (i+1)).style.top =  1*i + "px";
                 element.parentElement.style.top = posy + "px";
@@ -293,9 +353,9 @@ function startRace() {
                     document.getElementById("virus" + (k + 1)).parentElement.style.top = ((yMargin + wheight / 2 + yinc * i) + 70) + "px"; //((yMargin + wheight / 2 + yinc * i) + 30) + "px";
                     //  ctx.drawImage(nena,window.innerWidth-scrollPosition,(600)*(window.innerHeight/1963)+30,40,40); 
                 }
-                
+
                 // ctx.drawImage(virus, wwidth - virusPosition + 80, (yMargin + wheight / 2 + yinc * i) + 30, 40, 40);
-                if (virusPosition - posx - 200 <= 0 &&  !oneCuring) {
+                if (virusPosition - posx - 200 <= 0 && !oneCuring) {
                     // virus hit the target, we replace it
                     oneCuring = true;
                     clonedElement = element.cloneNode();
@@ -309,13 +369,13 @@ function startRace() {
                     // element.style.left = (topVal+120) + "px";
 
                     posRip = i;
-                   // element.parentNode.replaceChild(virusImg, element);
-                   window.setTimeout(restoreElement, 1000);
+                    // element.parentNode.replaceChild(virusImg, element);
+                    window.setTimeout(restoreElement, 1000);
                     restartVirus = true;
                 }
                 virusPosition = (virusPosition - 20);// % wwidth;// += 20;
             }
-//
+            //
 
             var record = FullData[i];
             WriteCountryLine(record, (yMargin + wheight / 2 + yinc * i) + 90, posx, factor);
@@ -332,7 +392,7 @@ function startRace() {
         for (j = 0; j < 1; j++) {
             document.getElementById("objImg" + (j + 1)).style.left = (wwidth - 100 - scrollPosition) + "px";
             //   document.getElementById("horse" + (i+1)).style.top =  1*i + "px";
-            document.getElementById("objImg" + (j + 1)).parentElement.style.top = (100+wheight / 2 + 2 * yMargin) + "px";
+            document.getElementById("objImg" + (j + 1)).parentElement.style.top = (100 + wheight / 2 + 2 * yMargin) + "px";
             //  ctx.drawImage(nena,window.innerWidth-scrollPosition,(600)*(window.innerHeight/1963)+30,40,40); 
         }
 
